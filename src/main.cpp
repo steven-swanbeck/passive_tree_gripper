c13346c7805b6db163bebdc3f1272c6a7b06fb88
steven-swanbeck <steven.swanbeck@gmail.com>
2023-04-20 23:18:35 -0500

    upkeep

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,9 @@
 #include <Stepper.h>
 
 // Define FSR pin:
-#define fsrpin A0
-#define swpin 2
-
-//Define variable to store sensor readings:
-int fsrreading; //Variable to store FSR value
+#define fsrpin1 A0
+#define fsrpin2 A1
+#define potpin1 A2
 
 // Define number of steps per revolution:
 const int stepsPerRevolution = 200;
@@ -14,100 +12,118 @@ const int stepsPerRevolution = 200;
 // Initialize the stepper library on pins 8 through 11:
 Stepper myStepper = Stepper(stepsPerRevolution, 3, 4, 5, 6);
 
-// Button stuffs
-bool bOld {1};
-bool bNew {};
-bool led {};
-unsigned long time = 0;
-unsigned long debounce = 20;
-
 //-------------------------------------------------------------
 // Settings
 int target_force {12}; // N
 float conversion_factor {0.02}; // N/bit(/8)
-int prop_gain {3};
+int prop_gain {1};
 float tolerance_window {0.15};
 
 int min_step_size {5};
 int max_step_size {200};
+
+bool activeFeedback {0};
+signed long currentRotation {0};
+
 //-------------------------------------------------------------
-
-
-
 // Function prototypes
-bool monitorButton ();
+bool setControlMode (); // todo Caleb
+int adjustSpeed (); // todo Jared
 float readForce ();
-int calculateStep (float current_force);
+int calculateStep (float current_force); // todo include derivative control
+int potentiometerControl (); // todo Steven
+bool checkLimits (); // todo Steven
+bool checkIntent (); // todo Steven
 
-
-
+//-------------------------------------------------------------
 void setup () {
   // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
 
   // Set the motor speed (RPMs):
-  myStepper.setSpeed(250);
-
-  // Button
-  pinMode(swpin, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  myStepper.setSpeed(1);
 }
 
 void loop () {
-  // bool grasp_mode = monitorButton();
-  bool grasp_mode = true;
+  // Check button to determine whether should be using remote control or autonomous
+  bool activeFeedback {setControlMode()};
 
-  Serial.print(grasp_mode);
+  int targetSpeed {adjustSpeed()};
 
-  if (grasp_mode == true) {
+  bool canContinue {checkLimits()}; 
+
+  if (activeFeedback == true) {
+    // read in force
     float current_force = readForce();
 
-    // Calculate and enact update
+    // use it to determine proportional update
     int step_size = calculateStep(current_force);
-    myStepper.step(step_size);
+
+    canContinue = checkIntent();
+    if (canContinue == true) {
+      // execute update
+      myStepper.step(step_size);
+    }
     delay(10);
+
   } else {
-    delay(50);
+    // int step_size = potentiometerControl();
+
+    // canContinue = checkIntent();
+    // if (canContinue == true) {
+    //   // execute update
+    //   myStepper.step(step_size);
+    // }
+    myStepper.step(10);
+    delay(10);
   }
+  delay(10);
 }
 
-
-
-bool monitorButton ()
+//-------------------------------------------------------------
+// todo Jared
+int adjustSpeed ()
 {
-  bNew = digitalRead(swpin);
-  if (bNew == HIGH && bOld == LOW && millis() - time > debounce)
-  {
-    led = !led;
-
-    time = millis();
-  }
-  digitalWrite(LED_BUILTIN, led);
-  bOld = bNew;
-
-  return led;
+  int speed {}; // 250 was max we could use before, but now we have a 12v power supply instead of the 9v battery so we can probably crank that up if desired
+  myStepper.setSpeed(speed);
+  return speed;
 }
 
+// todo Caleb
+bool setControlMode ()
+{
+  bool state {};
+  return state;
+}
+
+// todo Steven
+int potentiometerControl ()
+{
+  int target = analogRead(potpin1);
+  Serial.println(target);
+  return target;
+}
+
+// - DONE
 float readForce ()
 {
-  fsrreading = analogRead(fsrpin);
-  float current_force = fsrreading  * conversion_factor;
+  int fsrreading1 = analogRead(fsrpin1);
+  int fsrreading2 = analogRead(fsrpin2);
+  float current_force = conversion_factor * ((fsrreading1 + fsrreading2) / 2);
 
   Serial.print("Reading = ");
-  Serial.print(fsrreading);
-  Serial.print(",   ");
-  Serial.println(current_force);
+  Serial.print(current_force);
 
   return current_force;
 }
 
+ // todo ADD DERIVATIVE CONTROL!
 int calculateStep (float current_force)
 {
   int step_size {};
   if (((current_force < (1 - tolerance_window) * target_force) && (current_force < target_force)) || ((current_force > (1 + tolerance_window) * target_force) && (current_force > target_force))) {
     // step_size = prop_gain * (target_force - current_force);
-    step_size = ((target_force / conversion_factor) - (fsrreading - 0)) * (max_step_size - min_step_size) / (1023 - 0);
-    Serial.println("Actuating...");
+    step_size = prop_gain * ((target_force / conversion_factor) - (current_force / conversion_factor - 0)) * (max_step_size - min_step_size) / (1023 - 0); // ! not strict prop control currently
   }
   return step_size;
 }
